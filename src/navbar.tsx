2dbdadf8006888557e6c9bdd42405a712908f02b
Gabriel Collares <98597812+GabrielCollares@users.noreply.github.com>
2024-05-25 04:10:36 +0000

    Home page 1/2

---
+++ src/navbar.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './index.css'
+
+
+function  App() {
+
+  return (
+    <>
+  <section className="home">
+            <div className="home-text">
+                <h1 className="text-h1">Many know how to make money, but few know how to spend it.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod error veniam in voluptatem magni quisquam</p>
+                <a href="#" className="home-btn">Order yours</a>
+            </div>
+            <div className="home-img">
+                <img src="https://cdn.dribbble.com/userupload/7357224/file/original-02fbbd3cc8d9d871b176360e7e7de371.png?resize=1024x771" alt=""></img>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default App
