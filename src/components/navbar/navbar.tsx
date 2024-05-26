da7d7982981cecf8497dd2216d2baa8fbdcb5d0f
Gabriel GC <gabrielcccruz@hotmail.com>
2024-05-26 01:29:35 -0300

    Create navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+import './navbar.css'
+
+
+function  Navbar() {
+
+    return (
+      <>
+<div className="topnav">
+  <div className="topnav-right">
+    <a className="SignUp" href="/register">Sign Up</a>
+    <a className="SignIn " href="/login">Sign In</a>
+  </div>
+</div>
+
+    </>
+    )
+  }
+  
+  export default Navbar
+  
