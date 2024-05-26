37f29cab070c19819f1430686681d0b7be7205fa
Gabriel GC <gabrielcccruz@hotmail.com>
2024-05-26 01:30:24 -0300

    new pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+function LoginPage () {
+
+return (
+
+<>
+<h1>TESTE</h1>
+</>
+)
+}
+
+export default LoginPage
---
+++ src/pages/register.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+function RegisterPage () {
+
+return (
+
+<>
+<h1>TESTE2</h1>
+</>
+)
+}
+
+export default RegisterPage
