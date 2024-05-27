0226c670f096e60bb33daa54aec9539d05265df3
Gabriel GC <gabrielcccruz@hotmail.com>
2024-05-27 13:34:13 -0300

    change hp

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,27 @@
 import Navbar from '../components/navbar/navbar'
 import '../index.css'
 
-
-function  HomePage() {
+function HomePage() {
 
   return (
     <>
-    <Navbar/>
-  <section className="home">
-            <div className="home-text">
-                <h1 className="text-h1">Many know how to make money, but few know how to spend it.</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod error veniam in voluptatem magni quisquam</p>
-                <a href="#" className="home-btn">Get it now</a>
-            </div>
-            <div className="home-img">
-                <img src="https://cdn.dribbble.com/userupload/7357224/file/original-02fbbd3cc8d9d871b176360e7e7de371.png?resize=1024x771" alt=""></img>
-            </div>
-        </section>
+      <Navbar />
+      <section className="home">
+        <div className="home-text">
+          <h1 className="text-h1">Your Financial Future, Right at Your Fingertips.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod error veniam in voluptatem magni quisquam</p>
+          <a href="#" className="home-btn">Get it now</a>
+        </div>
+        <div className="home-img">
+          <img src="https://cdn.dribbble.com/users/697179/screenshots/17078664/media/e6651a82c7867d2a977a29c42a65fe9b.png?resize=1000x750&vertical=center" alt=""></img>
+        </div>
+      </section>
+
+      <section className="home2">
+        <h1>OI</h1>
+
+
+      </section>
     </>
   )
 }
